3f714a665234fa2fe8f89fd4dcc353713fde650b
Digias <86567479+Digias@users.noreply.github.com>
2025-05-07 18:51:19 +0200

    add notation service

--- SiwBooks/src/main/java/it/uniroma3/siw/service/BookService.java
+++ SiwBooks/src/main/java/it/uniroma3/siw/service/BookService.java
@@ -1,9 +1,13 @@
 package it.uniroma3.siw.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import it.uniroma3.siw.repository.BookRepository;
+import jakarta.transaction.Transactional;
 
+@Service
+@Transactional
 public class BookService {
 
 	@Autowired private BookRepository bookRepository;
--- SiwBooks/src/main/java/it/uniroma3/siw/service/ReviewService.java
+++ SiwBooks/src/main/java/it/uniroma3/siw/service/ReviewService.java
@@ -3,14 +3,12 @@ package it.uniroma3.siw.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import it.uniroma3.siw.repository.AuthorRepository;
+import it.uniroma3.siw.repository.ReviewRepository;
 import jakarta.transaction.Transactional;
 
 @Service
 @Transactional
-public class AuthorService {
-	
-	@Autowired private AuthorRepository authorRepository;
-	
-	
+public class ReviewService {
+
+	@Autowired private ReviewRepository reviewRepository;
 }
