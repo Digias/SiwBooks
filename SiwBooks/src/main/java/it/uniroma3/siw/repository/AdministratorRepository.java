7d1be2c5b7242424dfc7aa79c91bbe968b5f2cf1
Digias <86567479+Digias@users.noreply.github.com>
2025-05-07 19:22:02 +0200

    last repository

---
+++ SiwBooks/src/main/java/it/uniroma3/siw/repository/AdministratorRepository.java
@@ -0,0 +1,9 @@
+package it.uniroma3.siw.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.model.Administrator;
+
+public interface AdministratorRepository extends CrudRepository<Administrator, Long> {
+
+}
