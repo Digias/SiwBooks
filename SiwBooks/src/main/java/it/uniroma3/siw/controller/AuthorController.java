e7ab799b7fea5b701d35c5b50f693d209bc76012
Digias <86567479+Digias@users.noreply.github.com>
2025-05-07 18:45:45 +0200

    create all author class and interface

--- SiwBooks/src/main/java/it/uniroma3/siw/controller/AuthorController.java
+++ SiwBooks/src/main/java/it/uniroma3/siw/controller/AuthorController.java
@@ -1,8 +1,12 @@
 package it.uniroma3.siw.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import it.uniroma3.siw.service.AuthorService;
 
 @Controller
 public class AuthorController {
 
+	@Autowired private AuthorService authorService;
 }
---
+++ SiwBooks/src/main/java/it/uniroma3/siw/service/AuthorService.java
@@ -0,0 +1,12 @@
+package it.uniroma3.siw.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import it.uniroma3.siw.repository.AuthorRepository;
+
+public class AuthorService {
+	
+	@Autowired private AuthorRepository authorRepository;
+	
+	
+}
