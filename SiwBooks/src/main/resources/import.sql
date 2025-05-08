2d98e638dd9b3d2bc2e89347c62e30828a3d64fa
Digias <86567479+Digias@users.noreply.github.com>
2025-05-08 16:52:59 +0200

    Import book author

---
+++ SiwBooks/src/main/resources/import.sql
@@ -0,0 +1,80 @@
+
+--=======================================================================================================
+-- =========================== Books
+--=======================================================================================================
+INSERT INTO book (title, year_of_publication) VALUES ('Il nome della rosa', 1980);
+INSERT INTO book (title, year_of_publication) VALUES ('Good Omens', 1990);
+INSERT INTO book (title, year_of_publication) VALUES ('Guida galattica per autostoppisti', 1979);
+INSERT INTO book (title, year_of_publication) VALUES ('Il libro della giungla', 1894);
+INSERT INTO book (title, year_of_publication) VALUES ('Il Talismano', 1984);
+INSERT INTO book (title, year_of_publication) VALUES ('Il libro delle meraviglie per ragazzi e ragazze', 1857);
+INSERT INTO book (title, year_of_publication) VALUES ('Delitto e castigo', 1866);
+INSERT INTO book (title, year_of_publication) VALUES ('Il barone rampante', 1957);
+INSERT INTO book (title, year_of_publication) VALUES ('La coscienza di Zeno', 1923);
+INSERT INTO book (title, year_of_publication) VALUES ('Don Chisciotte della Mancia', 1605);
+INSERT INTO book (title, year_of_publication) VALUES ('I promessi sposi', 1827);
+INSERT INTO book (title, year_of_publication) VALUES ('Il piccolo principe', 1943);
+INSERT INTO book (title, year_of_publication) VALUES ('Harry Potter e la pietra filosofale', 1997);
+INSERT INTO book (title, year_of_publication) VALUES ('Il signore degli anelli', 1954);
+INSERT INTO book (title, year_of_publication) VALUES ('La metamorfosi', 1915);
+INSERT INTO book (title, year_of_publication) VALUES ('Il giovane Holden', 1951);
+INSERT INTO book (title, year_of_publication) VALUES ('Fahrenheit 451', 1953);
+INSERT INTO book (title, year_of_publication) VALUES ('Il grande Gatsby', 1925);
+INSERT INTO book (title, year_of_publication) VALUES ('Moby Dick', 1851);
+INSERT INTO book (title, year_of_publication) VALUES ('Guerra e pace', 1869);
+
+--=======================================================================================================
+-- =========================== Authors
+--=======================================================================================================
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('Umberto', 'Eco', '1932-01-05', '2016-02-19', 'Italiana');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('George', 'Orwell', '1903-06-25', '1950-01-21', 'Britannica');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('Gabriel', 'García Márquez', '1927-03-06', '2014-04-17', 'Colombiana');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('Giuseppe', 'Tomasi di Lampedusa', '1896-12-23', '1957-07-23', 'Italiana');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('Jane', 'Austen', '1775-12-16', '1817-07-18', 'Britannica');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('Fëdor', 'Dostoevskij', '1821-11-11', '1881-02-09', 'Russa');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('Italo', 'Calvino', '1923-10-15', '1985-09-19', 'Italiana');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('Italo', 'Svevo', '1861-12-19', '1928-09-13', 'Italiana');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('Miguel', 'de Cervantes', '1547-09-29', '1616-04-23', 'Spagnola');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('Alessandro', 'Manzoni', '1785-03-07', '1873-05-22', 'Italiana');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('Antoine', 'de Saint-Exupéry', '1900-06-29', '1944-07-31', 'Francese');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('J.K.', 'Rowling', '1965-07-31', NULL, 'Britannica');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('J.R.R.', 'Tolkien', '1892-01-03', '1973-09-02', 'Britannica');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('Franz', 'Kafka', '1883-07-03', '1924-06-03', 'Austro-Ungarica');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('J.D.', 'Salinger', '1919-01-01', '2010-01-27', 'Statunitense');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('Ray', 'Bradbury', '1920-08-22', '2012-06-05', 'Statunitense');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('F. Scott', 'Fitzgerald', '1896-09-24', '1940-12-21', 'Statunitense');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('Herman', 'Melville', '1819-08-01', '1891-09-28', 'Statunitense');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('Lev', 'Tolstoj', '1828-09-09', '1910-11-20', 'Russa');
+INSERT INTO author (name, surname, date_of_birth, date_of_death, nationality) VALUES ('Lewis', 'Carroll', '1832-01-27', '1898-01-14', 'Britannica');
+
+--=======================================================================================================
+-- =========================== Book - Author
+--=======================================================================================================
+-- Collegamenti libro 1 autore
+INSERT INTO book_authors (authors_id, books_id) VALUES (1, 1);  -- Il nome della rosa: Umberto Eco
+INSERT INTO book_authors (authors_id, books_id) VALUES (2, 2);  -- Good Omens: George Orwell
+INSERT INTO book_authors (authors_id, books_id) VALUES (3, 3);  -- Guida galattica per autostoppisti: Gabriel García Márquez
+INSERT INTO book_authors (authors_id, books_id) VALUES (4, 4);  -- Il libro della giungla: Giuseppe Tomasi di Lampedusa
+INSERT INTO book_authors (authors_id, books_id) VALUES (5, 5);  -- Il Talismano: Jane Austen
+
+-- Collegamenti libro con coautori
+INSERT INTO book_authors (authors_id, books_id) VALUES (6, 6);  -- Il libro delle meraviglie per ragazzi e ragazze: Fëdor Dostoevskij
+INSERT INTO book_authors (authors_id, books_id) VALUES (7, 7);  -- Delitto e castigo: Italo Calvino
+INSERT INTO book_authors (authors_id, books_id) VALUES (8, 8);  -- Il barone rampante: Italo Svevo
+INSERT INTO book_authors (authors_id, books_id) VALUES (9, 9);  -- La coscienza di Zeno: Miguel de Cervantes
+INSERT INTO book_authors (authors_id, books_id) VALUES (10, 10); -- Don Chisciotte della Mancia: Alessandro Manzoni
+
+-- Collegamenti autore con 5 libri
+-- George Orwell
+INSERT INTO book_authors (authors_id, books_id) VALUES (2, 11); -- 1984
+INSERT INTO book_authors (authors_id, books_id) VALUES (2, 12); -- Animal Farm
+INSERT INTO book_authors (authors_id, books_id) VALUES (2, 13); -- Homage to Catalonia
+INSERT INTO book_authors (authors_id, books_id) VALUES (2, 14); -- Down and Out in Paris and London
+INSERT INTO book_authors (authors_id, books_id) VALUES (2, 15); -- The Road to Wigan Pier
+
+-- J.K. Rowling
+INSERT INTO book_authors (authors_id, books_id) VALUES (11, 16); -- Harry Potter e la pietra filosofale
+INSERT INTO book_authors (authors_id, books_id) VALUES (11, 17); -- Harry Potter e la camera dei segreti
+INSERT INTO book_authors (authors_id, books_id) VALUES (11, 18); -- Harry Potter e il prigioniero di Azkaban
+INSERT INTO book_authors (authors_id, books_id) VALUES (11, 19); -- Harry Potter e il calice di fuoco
+INSERT INTO book_authors (authors_id, books_id) VALUES (11, 20); -- Harry Potter e l'ordine della fenice
