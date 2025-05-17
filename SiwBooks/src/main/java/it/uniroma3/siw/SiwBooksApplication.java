44a54e945bd0b2398869a65f5e355b486ee237d5
Digias <86567479+Digias@users.noreply.github.com>
2025-05-17 19:07:05 +0200

    add comment

--- SiwBooks/src/main/java/it/uniroma3/siw/SiwBooksApplication.java
+++ SiwBooks/src/main/java/it/uniroma3/siw/SiwBooksApplication.java
@@ -8,6 +8,9 @@ public class SiwBooksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SiwBooksApplication.class, args);
+		//per criptare password da passare in import.sql
+//		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//		System.out.println(encoder.encode("admin"));
 	}
 
 }
