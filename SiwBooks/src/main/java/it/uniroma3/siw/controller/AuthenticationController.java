cf00b3e063abc57d3146e7b5a6867f8547cc0942
Digias <86567479+Digias@users.noreply.github.com>
2025-05-20 16:43:39 +0200

    add confirm password and update css

--- SiwBooks/src/main/java/it/uniroma3/siw/controller/AuthenticationController.java
+++ SiwBooks/src/main/java/it/uniroma3/siw/controller/AuthenticationController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import it.uniroma3.siw.model.Credentials;
 import it.uniroma3.siw.model.Role;
@@ -21,20 +22,20 @@ import jakarta.validation.Valid;
 
 @Controller
 public class AuthenticationController {
-	
+
 	@Autowired
 	private CredentialsService credentialsService;
 
-    @Autowired
+	@Autowired
 	private UserService userService;
-	
+
 	@GetMapping(value = "/register") 
 	public String showRegisterForm (Model model) {
 		model.addAttribute("user", new User());
 		model.addAttribute("credentials", new Credentials());
 		return "formRegisterUser.html";
 	}
-	
+
 	@GetMapping(value = "/login") 
 	public String showLoginForm (Model model) {
 		return "formLogin";
@@ -44,7 +45,7 @@ public class AuthenticationController {
 	public String index(Model model) {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		if (authentication instanceof AnonymousAuthenticationToken) {
-	        return "index.html";
+			return "index.html";
 		}
 		else {		
 			UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -53,36 +54,40 @@ public class AuthenticationController {
 				return "index.html";
 			}
 		}
-        return "index.html";
+		return "index.html";
 	}
-		
-    @GetMapping(value = "/success")
-    public String defaultAfterLogin(Model model) {
-        
-    	UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    	Credentials credentials = credentialsService.getCredentials(userDetails.getUsername());
-    	if (credentials.getRole().equals(Role.ADMIN)) {
-            return "admin/indexAdmin.html";
-        }
-        return "index.html";
-    }
 
-    @PostMapping(value = { "/register" })
-    public String registerUser(@Valid @ModelAttribute("user") User user,
-                 BindingResult userBindingResult,
-                 @Valid @ModelAttribute("credentials") Credentials credentials,
-                 BindingResult credentialsBindingResult,
-                 Model model) {
+	@GetMapping(value = "/success")
+	public String defaultAfterLogin(Model model) {
 
-        if (!userBindingResult.hasErrors() && !credentialsBindingResult.hasErrors()) {
-            userService.saveUser(user);
-            credentials.setUser(user);
-            credentialsService.saveCredentials(credentials);
-            model.addAttribute("user", user);
-            return "registrationSuccessful";
-        }
+		UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Credentials credentials = credentialsService.getCredentials(userDetails.getUsername());
+		if (credentials.getRole().equals(Role.ADMIN)) {
+			return "admin/indexAdmin.html";
+		}
+		return "index.html";
+	}
 
-        return "formRegisterUser.html";
-    }
+	@PostMapping(value = { "/register" })
+		public String registerUser(@Valid @ModelAttribute("user") User user,
+				BindingResult userBindingResult,
+				@Valid @ModelAttribute("credentials") Credentials credentials,
+				@RequestParam("confirmPassword") String confirmPassword,
+				BindingResult credentialsBindingResult,
+				Model model) {
+		if(!credentials.getPassword().equals(confirmPassword)) {
+			credentialsBindingResult.rejectValue("password", "error.credentials", "Le password non coincidono");
+		}
+
+		if (!userBindingResult.hasErrors() && !credentialsBindingResult.hasErrors()) {
+			userService.saveUser(user);
+			credentials.setUser(user);
+			credentialsService.saveCredentials(credentials);
+			model.addAttribute("user", user);
+			return "registrationSuccessful";
+		}
+
+		return "formRegisterUser.html";
+	}
 
 }
