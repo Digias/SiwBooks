b23e131f90b0c8902e2af8bd32dee0d37a48b0bc
Digias <86567479+Digias@users.noreply.github.com>
2025-05-21 18:39:21 +0200

    add image mapping

--- SiwBooks/src/main/java/it/uniroma3/siw/controller/ImageController.java
+++ SiwBooks/src/main/java/it/uniroma3/siw/controller/ImageController.java
@@ -1,5 +1,7 @@
 package it.uniroma3.siw.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
@@ -8,9 +10,20 @@ import org.springframework.web.bind.annotation.*;
 import it.uniroma3.siw.model.Image;
 import it.uniroma3.siw.service.ImageService;
 
-@Controller
+@RestController
+@RequestMapping("/images")
 public class ImageController {
 
-	@Autowired private ImageService imageService;
+    @Autowired
+    private ImageService imageService;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<byte[]> getImage(@PathVariable Long id) {
+        Optional<Image> image = imageService.getImage(id);
+        return image.map(img -> ResponseEntity.ok()
+                        .contentType(MediaType.IMAGE_JPEG) // Usa IMAGE_PNG se necessario
+                        .body(img.getData()))
+                    .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
+
