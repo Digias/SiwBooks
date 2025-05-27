e901dd0b24236c420a692ec723dfe996f6d77be4
Digias <86567479+Digias@users.noreply.github.com>
2025-05-27 19:45:21 +0200

    some fix

--- SiwBooks/src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
+++ SiwBooks/src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
@@ -64,7 +64,7 @@ import javax.sql.DataSource;
                 .and().formLogin()
                 .loginPage("/login")
                 .permitAll()
-                .defaultSuccessUrl("/success", false)
+                .defaultSuccessUrl("/success", true)
                 .failureUrl("/login?error=true")
                 // LOGOUT: qui definiamo il logout
                 .and()
