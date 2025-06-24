628a33f57cc271b36685bb42dc5b3b7c4be68d7e
Digias <francescodigianvincenzo@gmail.com>
2025-06-24 15:45:20 +0200

    delete unused import

--- SiwBooks/src/main/java/it/uniroma3/siw/service/AuthorService.java
+++ SiwBooks/src/main/java/it/uniroma3/siw/service/AuthorService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import it.uniroma3.siw.model.Author;
